3b380669ad0fc0d6a3608fca44150d2c994ffd6b
Lakshya-Kapoor <Lakshya.Kapoor@iiitb.ac.in>
2024-11-29 22:37:36 +0530

    Add application properties and initial test class for Tuckos application

---
+++ backend/src/test/java/com/tuckos/TuckosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuckos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuckosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

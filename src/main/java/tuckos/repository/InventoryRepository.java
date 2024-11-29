99ed25a63a42e4095365902723caaa75211ed221
Jayesh_Pandit <jayesh.pandit@iiitb.ac.in>
2024-11-29 22:07:53 +0530

    updating Inventory

--- src/main/java/tuckos/repository/InventoryRepository.java
+++ src/main/java/tuckos/repository/InventoryRepository.java
@@ -1,10 +1,15 @@
 package tuckos.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import tuckos.entity.Inventory;
 import tuckos.entity.Item;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Item> {
-
+    @Query("SELECT i FROM Inventory i WHERE i.item.itemId = :itemId")
+    Optional<Inventory> findByItemId(@Param("itemId") Long itemId);
 }
